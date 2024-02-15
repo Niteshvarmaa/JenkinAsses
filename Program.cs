dcbc8e5f485a7e0b392eea8d20b1abba93eb1a16
Niteshvarmaa <niteshvarma126@gmail.com>
2024-02-15 15:43:13 +0530

    Final Commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ADDITION OF TWO NUMBERS");
+int num1 = 3;
+int num2 = 5;
+Console.WriteLine("{0} + {1}", num1, num2, (num1 + num2));
